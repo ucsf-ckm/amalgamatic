9786dddb919b610adb50bdb020f90e277c8e5693
Rich Trott <rtrott@gmail.com>
2014-09-16 21:22:35 -0700

    allow programmatic adding of collectors for #2

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-exports.search = require('./lib/search');
+var async = require('async');
+
+var collections = {};
+
+exports.search = function (req, res) {
+  'use strict';
+
+  var requestedCollections;
+  if (! req.query.c || ! req.query.c instanceof Array) {
+    requestedCollections = Object.keys(collections);
+  } else {
+    requestedCollections = req.query.c;
+  }
+
+  var results = {};
+
+  var iterator = function (c, done) {
+    if (c in collections) {
+      collections[c].search(req.query.q, function (value) {
+        results[c] = {data: value.data};
+        done();
+      });
+    } else {
+      results[c] = {error: 'Collection "' + c + '" does not exist'};
+      done();
+    }
+  };
+
+  var callback = function () {
+    res.json(results);
+  };
+
+  async.each(requestedCollections, iterator, callback);
+};
+
+exports.add = function (collection) {
+  collections[collection] = require(__dirname + '/lib/' + collection);
+};
--- test/index.js
+++ test/index.js
@@ -1,6 +1,8 @@
 /*jshint expr: true*/
 
 var amalgamatic = require('../index.js');
+amalgamatic.add('sfx');
+
 
 var Lab = require('lab');
 var lab = exports.lab = Lab.script();
@@ -16,3 +18,71 @@ describe('amalgamatic', function () {
 		done();
 	});
 });
+
+
+var nock = require('nock');
+
+var EventEmitter = require('events').EventEmitter;
+var emitter = new EventEmitter();
+
+var res = {
+	json: function(value) {
+		emitter.emit('end', value);
+	}
+};
+
+describe('search', function () {
+
+	var searchHelper = function (q, c, callback) {
+		var req = {
+			query: {
+				q: q,
+				c: c
+			}
+		};
+
+		emitter.once('end', callback);
+
+		amalgamatic.search(req, res);
+	};
+
+	it('returns only specified collection', function (done) {
+		nock('http://ucelinks.cdlib.org:8888')
+			.get('/sfx_ucsf/az?param_textSearchType_value=startsWith&param_pattern_value=medicine')
+			.reply('200', '<a class="Results" href="#">Medicine</a><a class="Results" href="#">Medicine</a>');
+
+		searchHelper('medicine', ['sfx'], function (results) {
+			expect(results.sfx.data.length > 0).to.be.true;
+			expect(results.sfx.error).to.be.undefined;
+			done();
+		});
+	});
+
+	it('returns an error if an invalid collection is specified', function (done) {
+		searchHelper('medicine', ['fhqwhgads'], function (results) {
+			expect(results.fhqwhgads.data).to.be.undefined;
+			expect(results.fhqwhgads.error).to.equal('Collection "fhqwhgads" does not exist');
+			done();
+		});
+	});
+
+	it('returns multiple collections if specified', function (done) {
+		nock('http://ucelinks.cdlib.org:8888')
+			.get('/sfx_ucsf/az?param_textSearchType_value=startsWith&param_pattern_value=medicine')
+			.reply('200', '<a class="Results" href="#">Medicine</a><a class="Results" href="#">Medicine</a>');
+
+		searchHelper('medicine', ['sfx', 'fhqwhgads'], function (results) {
+			expect(results.sfx.data).to.be.ok;
+			expect(results.fhqwhgads.error).to.be.ok;
+			done();
+		});
+	});
+
+	it('returns all collections if no collection specified', function (done) {
+		searchHelper('medicine', null, function (results) {
+			expect(results.sfx.data).to.be.ok;
+			done();
+		});
+	});
+});
+
